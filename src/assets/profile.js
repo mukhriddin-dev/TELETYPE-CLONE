7db51112116b3561ae3384a961ede066e7b4f7ac
mukhriddin-dev <khodievm@gamil.com>
2023-07-28 10:51:43 +0500

    FINAL

--- src/assets/profile.js
+++ src/assets/profile.js
@@ -34,7 +34,8 @@ $("#user_title").innerText = `Blogs ${localStorage.getItem("username")}`;
 // ----------------- auth guards --------------------------------
 
 window.addEventListener("DOMContentLoaded", () => {
-    if (!localStorage.getItem("token")) {
+    if (localStorage.getItem("token") || localStorage.getItem("user")) {
+    } else {
         window.location.href = "./index.html";
     }
 });
@@ -45,16 +46,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
 let id = localStorage.getItem("user");
 
+$("#posts").innerHTML = "<div class='loader_wrapper'><span class='loader'></span></div>";
 async function getUser() {
     try {
         const response = await fetch(`https://nest-blog-qdsa.onrender.com/api/user/${id}`);
         const result = await response.json();
         console.log(result);
-        dataRender(result);
 
-        listRender(result.blog, "posts");
-        listRender(result.followers, "followers");
-        listRender(result.followings, "following");
+        if (result) {
+            $("#posts").innerHTML = "";
+            dataRender(result);
+            listRender(result.blog, "posts");
+            listRender(result.followers, "followers");
+            followRender(result.followings);
+        }
     } catch (err) {
         console.log(err.message);
     } finally {
@@ -75,11 +80,14 @@ function dataRender(state) {
 }
 
 function listRender(state, selector) {
+    console.log(state);
     if (state.length) {
         state?.forEach((el) => {
             const card = document.createElement("div");
             card.classList.add("card");
-            card.innerHTML = `
+
+            if (localStorage.getItem("token") && localStorage.getItem("user_id") === localStorage.getItem("user")) {
+                card.innerHTML = `
                      <div class="post_item border relative shadow-md hover:shadow-xl duration-150 font-['inter'] rounded-lg p-4">
                           
                            <div class="flex absolute right-3 gap-x-3">
@@ -104,7 +112,7 @@ function listRender(state, selector) {
                             </p>
 
      
-                            <strong class="mb-[10px] leading-[25.5px]"> ● ${localStorage.getItem("username")}</strong>
+                            <strong class="mb-[10px] leading-[25.5px]"> </strong>
 
                             <div class="flex items-center gap-x-3 my-4">
                                 <span>${el.createdAt.substring(0, 10)}</span>
@@ -113,10 +121,53 @@ function listRender(state, selector) {
                             </div>
                         </div>
                 `;
+            } else {
+                card.innerHTML = `
+                     <div class="post_item border relative shadow-md hover:shadow-xl duration-150 font-['inter'] rounded-lg p-4">
+                          
+                          
+
+
+                            <h2 class="post__title text-3xl font-bold leading-[39px] mb-5">
+                               ${el.title}
+                            </h2>
+
+                            <p class="post__text mb-[25.5px] leading-[25.5px] text-[17px] font-normal">
+                                ${el.body?.substring(0, 256)} . . .
+                            </p>
+
+     
+                            <strong class="mb-[10px] leading-[25.5px]"> </strong>
+
+                            <div class="flex items-center gap-x-3 my-4">
+                                <span>${el.createdAt.substring(0, 10)}</span>
+                                <i class="bx bx-show"></i>
+                                <span>${el.views}</span>
+                            </div>
+                        </div>
+                `;
+            }
+
             $("#" + selector).append(card);
         });
     } else {
         $("#" + selector).innerHTML = `<h1 class='text-center'>${selector.toUpperCase()} NOT FOUND</h1>`;
+    }
+}
+// --------------------- Following and followers redner functions
+
+function followRender(state) {
+    if (state.length) {
+        state?.forEach((el) => {
+            console.log(el.following);
+            const followItem = document.createElement("div");
+            followItem.classList.add("card", "p-3", "m-2", "shadow");
+            followItem.innerHTML = `<h1>${el?.following?.full_name}</h1>
+            <p> ${el?.following?.username}</p>`;
+            $("#following").append(followItem);
+        });
+    } else {
+        $("#following").innerHTML="<h1>NOT FOUND</h1>"
     }
 }
 
@@ -146,4 +197,26 @@ $("#posts").addEventListener("click", (e) => {
         let uniqueID = e.target.getAttribute("data-del");
         deletePosts(uniqueID);
     }
+});
+
+function addFollow() {
+    let userid = localStorage.getItem("user");
+    return fetch("https://nest-blog-qdsa.onrender.com/api/follow", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({following_id: userid}),
+    });
+}
+
+$("#follow_btn").addEventListener("click", (e) => {
+    addFollow()
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+    setTimeout(() => {
+        // window.location.reload();
+    }, 1500);
 });
--- src/index.js
+++ src/index.js
@@ -54,6 +54,8 @@ $("#add_btn").addEventListener("click", () => {
 
 // -------------------- authorization actions  --------------------
 
+
+
 $("#signup").addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -103,7 +105,9 @@ $("#signin").addEventListener("click", (e) => {
         .then((result) => {
             localStorage.setItem("token", result.token);
             localStorage.setItem("user", result.user.id);
+            localStorage.setItem("user_id", result.user.id);
             localStorage.setItem("username", result?.user?.full_name);
+
             window.location.href = "./profile.html";
 
             if (result.statusCode == "400") {
@@ -129,6 +133,7 @@ function authChek() {
         $("#user_info").textContent = localStorage.getItem("username");
         $("#auth").classList.add("hidden");
         $("#addpost").classList.remove("hidden");
+        $("#add_btn").classList.remove("hidden");
     } else {
         $(".menu").classList.add("hidden");
         $(".dropdown").classList.add("hidden");
@@ -136,6 +141,7 @@ function authChek() {
 
         $("#auth").classList.remove("hidden");
         $("#addpost").classList.add("hidden");
+        $("#add_btn").classList.add("hidden");
     }
 }
 
@@ -161,7 +167,7 @@ function addPost() {
 
     if (newBlog.title.trim().length === 0 || newBlog.body.trim().length === 0) {
         alert("Please fill post title and body");
-       } else{
+    } else {
         fetch("https://nest-blog-qdsa.onrender.com/api/blog", {
             method: "POST",
             headers: {
@@ -180,3 +186,84 @@ $("#save").addEventListener("click", () => {
     addPost();
 });
 
+// ------------------------ BLOG POST END ------------------------
+
+// ----------------------- ALL BLOGS ----------------
+$(".post_wrapper").innerHTML = "<div class='loader_wrapper'><span class='loader'></span></div>";
+
+async function getAllPosts() {
+    try {
+        const response = await fetch(`https://nest-blog-qdsa.onrender.com/api/blog`);
+        const results = await response.json();
+        if (results.length) {
+            $(".post_wrapper").innerHTML = "";
+            listRender(results);
+        } else {
+            $(".post_wrapper").innerHTML = "<h1>DATA NOT FOUND</h1>";
+        }
+    } catch (err) {
+        // alert(err.message);
+    } finally {
+        console.log("LOADED ALL POSTS");
+    }
+}
+
+function listRender(state) {
+    console.log(state);
+    if (state.length) {
+        state?.forEach((el) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                     <div class="post_item border relative shadow-md hover:shadow-xl duration-150 font-['inter'] rounded-lg p-4">
+                          
+                         
+
+
+                            <h2 class="post__title text-3xl font-bold leading-[39px] mb-5">
+                               ${el.title}
+                            </h2>
+
+                            <p class="post__text mb-[25.5px] leading-[25.5px] text-[17px] font-normal">
+                                ${el.body?.substring(0, 256)} . . .
+                            </p>
+
+     
+                            <a class="user" href="./profile.html" target="_blank"   data-user="${el?.user?.id}">
+                               <strong class="mb-[10px]  user leading-[25.5px]" data-user="${el?.user?.id}"> ● ${
+                el?.user?.full_name
+            }</strong>
+                            </a>
+
+                            <div class="flex items-center gap-x-3 my-4">
+                                <span>${el.createdAt.substring(0, 10)}</span>
+                                <i class="bx bx-show"></i>
+                                <span>${el.views}</span>
+                            </div>
+                        </div>
+                `;
+            $(".post_wrapper").append(card);
+        });
+    } else {
+        $(".post_wrapper").innerHTML = `<h1 class='text-center'>${selector.toUpperCase()} NOT FOUND</h1>`;
+    }
+}
+
+getAllPosts();
+
+$(".post_wrapper").addEventListener("click", (e) => {
+    if (e.target.classList.contains("user")) {
+        console.log(e.target.textContent);
+        let userId = e.target.getAttribute("data-user");
+        console.log(userId);
+        localStorage.setItem("user", userId);
+    }
+});
+
+// ------------- redirect to my blog -------------
+
+$$(".user_id").forEach((item) => {
+    item.addEventListener("click", () => {
+        localStorage.setItem("user", localStorage.getItem("user_id"));
+    });
+});
